1974a2b300bda9432cfa74e4f1e81bcedd2c6f7d
anmol <github@datamol.org>
2019-04-16 18:33:30 +0530

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# shadowsocks
-A secure socks5 proxy, designed to protect your Internet traffic.
+# Shadowsocks for Yunohost
+The Shadowsocks local component  acts like a traditional SOCKS5 server and provides proxy service to clients.  It encrypts and forwards data streams and packets from the client to the Shadowsocks remote component, which decrypts and forwards to the target. Replies from target are similarly encrypted and relayed eventually returns to the original client.
+
+[![Integration level](https://dash.yunohost.org/integration/shadowsocks.svg)](https://dash.yunohost.org/appci/app/shadowsocks)  
+[![Install shadowsocks with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=shadowsocks)
+
+## Limitations
+
+NO Ldap support yet.
+
+## Important Links
+
+ * YunoHost website: https://yunohost.org/
+ * Shadowsocks website: https://shadowsocks.org/en/index.html
+ 
+#### Supported architectures
+
+* x86-64b - [![Build Status](https://ci-apps.yunohost.org/ci/logs/shadowsocks%20%28Community%29.svg)](https://ci-apps.yunohost.org/ci/apps/shadowsocks/)
+* ARMv8-A - [![Build Status](https://ci-apps-arm.yunohost.org/ci/logs/shadowsocks%20%28Community%29.svg)](https://ci-apps-arm.yunohost.org/ci/apps/shadowsocks/)
+* Jessie x86-64b - [![Build Status](https://ci-stretch.nohost.me/ci/logs/shadowsocks%20%28Community%29.svg)](https://ci-stretch.nohost.me/ci/apps/shadowsocks/)
+
---
+++ scripts/_common.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="install python python-dev python-setuptools openssl libssl-dev curl wget unzip gcc automake autoconf make libtool"
+
+# libsodium version for temp folder
+libsodium_file="libsodium-1.0.17"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+# Get public IP address
+get_ip(){
+    local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
+    [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipinfo.io/ip )
+    [ ! -z ${IP} ] && echo ${IP} || echo
+}
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
+
+
+ynh_smart_mktemp () {
+        local min_size="${1:-300}"
+        # Transform the minimum size from megabytes to kilobytes
+        min_size=$(( $min_size * 1024 ))
+
+        # Check if there's enough free space in a directory
+        is_there_enough_space () {
+                local free_space=$(df --output=avail "$1" | sed 1d)
+                test $free_space -ge $min_size
+        }
+
+        if is_there_enough_space /tmp; then
+                local tmpdir=/tmp
+        elif is_there_enough_space /var; then
+                local tmpdir=/var
+        elif is_there_enough_space /; then
+                local tmpdir=/   
+        elif is_there_enough_space /home; then
+                local tmpdir=/home
+        else
+		ynh_die "Insufficient free space to continue..."
+        fi
+
+        echo "$(sudo mktemp --directory --tmpdir="$tmpdir")"
+}
